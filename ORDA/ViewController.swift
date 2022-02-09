1e3eb156ab0ac07ffa73c5def5c76eaeff0d09b5
Yury Sychyk <kauz@tut.by>
2022-02-09 17:17:17 +0300

    end

--- ORDA/ViewController.swift
+++ ORDA/ViewController.swift
@@ -6,11 +6,15 @@
 //
 
 import UIKit
-
+import Foundation
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webField: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        webField.loadRequest(URLRequest(url: URL(string:"https://orda.of.by/.map/?52.539732,24.984361&m=kdwr/14,gm/14&poi=gb/h")!))
         // Do any additional setup after loading the view.
     }
 
---
+++ ORDA/WebView.swift
@@ -0,0 +1,8 @@
+//
+//  WebView.swift
+//  ORDA
+//
+//  Created by Yury Sychyk on 09.02.2022.
+//
+
+import Foundation
